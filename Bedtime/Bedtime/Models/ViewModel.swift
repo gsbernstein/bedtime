88d331766ac665053ece8fe2ff2daead6829a63e
Greg <greg@vida.com>
2026-08-13 21:38:34 -0700

    rename and use helper

--- Bedtime/Bedtime/Models/ViewModel.swift
+++ Bedtime/Bedtime/Models/ViewModel.swift
@@ -114,22 +114,22 @@ class ViewModel {
 
         // Calculate how much sleep we need tonight
         // If we're in debt, we need extra sleep to catch up
-        var totalSleepNeeded = sleepGoal - sleepBank.currentBalance
-        
+        var totalHoursNeeded = sleepGoal - sleepBank.currentBalance
+
         // Generate reason
         let reason: String?
         if sleepBank.averageHours == nil {
             reason = "No data so far, just aim for your goal"
-        } else if totalSleepNeeded > maxSleepHours {
-            totalSleepNeeded = maxSleepHours
+        } else if totalHoursNeeded > maxSleepHours {
+            totalHoursNeeded = maxSleepHours
             reason = nil
         } else if sleepBank.isInDebt {
             let debtHours = sleepBank.debtHours
-            let needed = TimeFormatter.formatHours(totalSleepNeeded, style: durationStyle)
+            let needed = TimeFormatter.formatHours(totalHoursNeeded, style: durationStyle)
             let debt = TimeFormatter.formatHours(debtHours, style: durationStyle)
             reason = "You need \(needed) tonight to catch up on your \(debt) sleep debt."
         } else {
-            totalSleepNeeded = sleepGoal
+            totalHoursNeeded = sleepGoal
             let goal = TimeFormatter.formatHours(
                 sleepGoal,
                 style: durationStyle,
@@ -138,9 +138,9 @@ class ViewModel {
             reason = "You're ahead of the game! Aim for at least \(goal) tonight."
         }
 
-        let sleepNeededMinutes = Int((totalSleepNeeded * 60).rounded())
-        let recommendedBedtime = calendar.date(byAdding: .minute, value: -sleepNeededMinutes, to: wakeTime) ?? wakeTime.addingTimeInterval(-totalSleepNeeded * 60 * 60)
-        
+        let sleepNeededMinutes = Int((totalHoursNeeded * 60).rounded())
+        let recommendedBedtime = calendar.date(byAdding: .minute, value: -sleepNeededMinutes, to: wakeTime) ?? wakeTime.addingTimeInterval(.hours(-totalHoursNeeded))
+
         return BedtimeRecommendation(
             recommendedBedtime: recommendedBedtime,
             wakeTime: wakeTime,
